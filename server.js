ac55a9150eb86c3cd0439ed1071b69d18a9dabbe
Jonathan Dorety <kyullog@gmail.com>
2019-04-11 00:02:13 -0400

    Add repair item GET endpoint

--- server.js
+++ server.js
@@ -13,10 +13,25 @@ server.get("/item/:id", async (req, res) => {
   try {
     const item = await db.getItem(id);
     res.status(200).json(enhancer.get(item));
-    console.log(item);
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Error retrieving item" });
+  }
+});
+
+server.get("/item/:id/repair", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const item = await db.getItem(id);
+    const repairedItem = enhancer.repair(item);
+    const updatedItem = await db.update(id, repairedItem);
+    if (updatedItem) {
+      const returnItem = await db.getItem(id);
+      res.status(200).json(enhancer.get(returnItem));
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "There was a problem repairing your item" });
   }
 });
 
