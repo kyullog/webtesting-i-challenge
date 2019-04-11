8cf9c148b3ee5bc5de495a7246e4c94503196edd
Jonathan Dorety <kyullog@gmail.com>
2019-04-10 21:38:45 -0400

    Implement repair function and tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
+const { oldSword, magicSword, oldShield } = require("../items.js");
+
+const { repair } = enhancer;
 // test away!
+describe("the enhancement suite", () => {
+  describe("the repair function", () => {
+    it("should return a new item with durability of 100", () => {
+      expect(repair(oldSword)).toEqual({ ...oldSword, durability: 100 });
+      expect(repair(magicSword)).toEqual({ ...magicSword, durability: 100 });
+    });
+  });
+});
