97582b4a142b65c0d0fe8b3173c04f22e4d59948
Jonathan Dorety <kyullog@gmail.com>
2019-04-11 00:02:02 -0400

    Create update item helper function

--- database/helpers.js
+++ database/helpers.js
@@ -1,11 +1,18 @@
 const db = require("./dbConfig.js");
 
 module.exports = {
-  getItem
+  getItem,
+  update
 };
 
 function getItem(id) {
   return db("items")
     .where({ id })
     .first();
+}
+
+function update(id, item) {
+  return db("items")
+    .where({ id })
+    .update({ ...item });
 }
