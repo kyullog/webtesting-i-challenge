0d037032115d64d3c843b617dee276aa6fcb0e8e
Jonathan Dorety <kyullog@gmail.com>
2019-04-10 22:25:59 -0400

    Implement enhance fail function and tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -14,6 +14,19 @@ function succeed(item) {
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else {
+    if (item.enhancement > 16) {
+      return {
+        ...item,
+        durability: item.durability - 10,
+        enhancement: item.enhancement - 1
+      };
+    } else {
+      return { ...item, durability: item.durability - 10 };
+    }
+  }
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -6,7 +6,7 @@ const {
   ultimateSpear
 } = require("../items.js");
 
-const { repair, succeed } = enhancer;
+const { repair, succeed, fail } = enhancer;
 // test away!
 describe("the repair function", () => {
   it("should return a new item with durability of 100", () => {
@@ -19,8 +19,24 @@ describe("the enhancement suite", () => {
   describe("a successful enhancement", () => {
     it(`should increase enhancement by 1, unless enhancment is 20, without changing durability`, () => {
       expect(succeed(oldSword)).toEqual({ ...oldSword, enhancement: 5 });
+      expect(succeed(magicSword)).toEqual({ ...magicSword, enhancement: 17 });
       expect(succeed(ultimateSpear)).toEqual({ ...ultimateSpear });
-      expect(succeed(oldShield)).toEqual({ ...oldShield, enhancement: 2 });
+      expect(succeed(oldShield)).toEqual({ ...oldShield, enhancement: 16 });
+    });
+  });
+  describe("a failed enhancement", () => {
+    it("decreases durability by 5 if enhancement < 15, by 10 if enhancement > 15, and will decrease enhancement by 1 if enhancement > 16", () => {
+      expect(fail(oldSword)).toEqual({ ...oldSword, durability: 12 });
+      expect(fail(oldShield)).toEqual({ ...oldShield, durability: 35 });
+      expect(fail(magicSword)).toEqual({
+        ...magicSword,
+        durability: 58
+      });
+      expect(fail(ultimateSpear)).toEqual({
+        ...ultimateSpear,
+        enhancement: 19,
+        durability: 90
+      });
     });
   });
 });
