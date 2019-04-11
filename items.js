d088066208df05b5228b0bbc553b4aa42576965a
Jonathan Dorety <kyullog@gmail.com>
2019-04-10 22:26:10 -0400

    Create stick

--- items.js
+++ items.js
@@ -7,8 +7,9 @@ function itemMaker(name, enhancement, durability) {
 }
 
 module.exports = {
+  stick: itemMaker("Wooden Stick", 0, 10),
   oldSword: itemMaker("Battered Bastard Sword", 4, 17),
+  oldShield: itemMaker("Wood Shield", 15, 45),
   magicSword: itemMaker("Sting", 16, 68),
-  oldShield: itemMaker("Wood Shield", 1, 45),
   ultimateSpear: itemMaker("Platinum Pike", 20, 100)
 };
